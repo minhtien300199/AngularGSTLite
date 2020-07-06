dee4f03e01499bcd0e183b39f4e822e27ff44929
Vo Minh Hieu <43235486+minhhieu023@users.noreply.github.com>
2020-07-06 12:19:19 +0700

    addproduct

---
+++ src/app/add-procduct/model.ts
@@ -0,0 +1,15 @@
+export class Product {
+    id: Int32Array;
+    productName: string;
+    description: string;
+    craeteAt: Date;
+    isDisable: boolean;
+    unitPrice: string;
+    unitInStock: Int16Array;
+    productCondition: string;
+    manufacturer: string;
+    imgPath: string;
+    productGroupId: Int32Array;
+    userId: Int32Array;
+    // tslint:disable-next-line:eofline
+}
